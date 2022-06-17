dd2eb7b06ca2b7660ee09aebf0349181b7314292
george <georgeslight@gmail.com>
2022-06-17 14:37:49 +0200

    formatting

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-6">
+            <!-- <div class="col-6">
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row g-0">
                         <div class="col-md-4">
@@ -17,18 +17,22 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-6">
-                <div class="card mb-3" style="max-width: 540px;">
+            </div> -->
+            <div class="col">
+                <div class="card mx-auto mt-3" style="width: 1000px;">
                     <div class="row g-0">
                         <div class="col-md-4">
-                            <img alt="Patrick" src="../assets/Patrick.jpeg" width="180" height="200">
+                            <img alt="Patrick" src="../assets/buffalomeatballs-03.webp" class="img-fluid" width="350"
+                                height="230">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Vegane Buffalo Meatballs mit Schnittlauch-Dip</h5>
+                                <p class="card-text">Als ich noch nicht Veganerin war, spielten Fleischbällchen in
+                                    meinem Leben eigentlich keine große Rolle. Mittlerweile sieht das anders aus und ich
+                                    LIEBE vegane, herzhafte Bällchen in jeglichen Varianten. Auf unserem Blog sieht man
+                                    sie deshalb auch in unterschiedlichsten Zusammensetzungen: auf Basis von
+                                    Kidneybohnen... </p>
                                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                             </div>
                         </div>
