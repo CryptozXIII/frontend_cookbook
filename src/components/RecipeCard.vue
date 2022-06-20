ba620f9367cb81632d1f8c715768a4b35d9d3e3f
george <georgeslight@gmail.com>
2022-06-20 11:24:23 +0200

    add .env files

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,39 +1,18 @@
 <template>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <!-- <div class="col-6">
-                <div class="card mb-3" style="max-width: 540px;">
+            <!-- Dynamische View -->
+            <div class="col" v-for="recipe in recipes" :key="recipe.id">
+                <div class="card mx-auto mt-3" style="width: 1100px;">
                     <div class="row g-0">
                         <div class="col-md-4">
-                            <img alt="Patrick" src="../assets/Patrick.jpeg" width="180" height="200">
+                            <img :alt="recipe.title" :src="require('@/assets/' + recipe.image)" class="img-fluid">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-            <div class="col">
-                <div class="card mx-auto mt-3" style="width: 1000px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img alt="Patrick" src="../assets/buffalomeatballs-03.webp" class="img-fluid" width="350"
-                                height="230">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Vegane Buffalo Meatballs mit Schnittlauch-Dip</h5>
-                                <p class="card-text">Als ich noch nicht Veganerin war, spielten Fleischbällchen in
-                                    meinem Leben eigentlich keine große Rolle. Mittlerweile sieht das anders aus und ich
-                                    LIEBE vegane, herzhafte Bällchen in jeglichen Varianten. Auf unserem Blog sieht man
-                                    sie deshalb auch in unterschiedlichsten Zusammensetzungen: auf Basis von
-                                    Kidneybohnen... </p>
-                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                <h5 class="card-title">{{ recipe.title }}</h5>
+                                <p class="card-text">{{ recipe.summary }}</p>
+                                <p class="card-text"><small class="text-muted">Likes: {{ recipe.likes }}</small></p>
                             </div>
                         </div>
                     </div>
@@ -45,7 +24,26 @@
 
 <script>
 export default {
+  name: 'RecipesCard',
+  data () {
+    return {
+      recipes: []
+    }
+  },
+  mounted () {
+    const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/recipeInformation/1'
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    }
 
+    fetch(endpoint, requestOptions)
+      .then(response => response.json())
+      .then(result => result.forEach(recipe => {
+        this.recipe.push(recipe)
+      }))
+      .catch(error => console.log('error', error))
+  }
 }
 </script>
 
