e3a08edbc0b583c1e78bc4d7834930f1ee8ae6e3
George Slight <georgeslight@gmail.com>
2022-06-28 15:43:08 +0200

    deploy fixed

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <!-- Dynamische View -->
       <div v-for="recipe in recipes" :key="recipe.id" class="col">
-        <div class="card mx-auto mt-3" style="width: 58rem; height: 15rem">
+        <div class="card mx-auto mt-3" style="width: 60rem; height: 15rem">
           <div class="row g-0">
             <div class="col-md-4">
               <img :alt="recipe.title" :src="recipe.image" class="img-fluid" />
@@ -11,11 +11,7 @@
             <div class="col-md-8">
               <div class="card-body">
                 <h5 class="card-title" v-html="recipe.title"></h5>
-                <p
-                  class="card-text"
-                  style="font-size: small"
-                  v-html="recipe.summary"
-                ></p>
+                <p class="card-text" style="font-size: small" v-html="recipe.summary"></p>
                 <p class="card-text">
                   <small class="text-muted">Likes: {{ recipe.likes }}</small>
                 </p>
@@ -40,30 +36,47 @@ export default {
     }
   },
   mounted () {
-    this.updateCards()
-  },
-  methods: {
-    updateCards () {
-      const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      }
-
-      fetch(
-        'http://localhost:8080/recipeByIngredients/'.concat(this.ingredients),
-        requestOptions
-      )
-        .then((response) => response.json())
-        .then((result) =>
-          result.forEach((recipe) => {
-            this.recipes.push(recipe)
-          })
-        )
-        .catch((error) => console.log('error', error))
-
-      console.log(this.recipes)
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
     }
+
+    fetch(
+      'http://localhost:8080/recipeByIngredients/' + this.ingredients,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.forEach((recipe) => {
+          this.recipes.push(recipe)
+        })
+      )
+      .catch((error) => console.log('error', error))
+
+    console.log(this.recipes)
   }
+  // methods: {
+  //   updateCards () {
+  //     const requestOptions = {
+  //       method: 'GET',
+  //       redirect: 'follow'
+  //     }
+  //
+  //     fetch(
+  //       'http://localhost:8080/recipeByIngredients/' + this.ingredients,
+  //       requestOptions
+  //     )
+  //       .then((response) => response.json())
+  //       .then((result) =>
+  //         result.forEach((recipe) => {
+  //           this.recipes.push(recipe)
+  //         })
+  //       )
+  //       .catch((error) => console.log('error', error))
+  //
+  //     console.log(this.recipes)
+  //   }
+  // }
 }
 </script>
 
