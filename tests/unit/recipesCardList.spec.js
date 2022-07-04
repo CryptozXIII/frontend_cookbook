3514d599a5b552a5b022c59d71546016e1cec509
Hoa <DucHoa.Le@Student.HTW-Berlin.de>
2022-07-04 16:36:34 +0200

    test3

--- tests/unit/recipesCardList.spec.js
+++ tests/unit/recipesCardList.spec.js
@@ -7,7 +7,7 @@ describe("testing Recipescardlist.vue", ()=>{
     //when
     const wrapper = mount(RecipesCardList,{
       propsData:{
-        recipe: [
+        recipes: [
           {
             "id": 4,
             "name": "Gnocchi in Walnusssoße und Salbei-Butter",
@@ -137,6 +137,195 @@ describe("testing Recipescardlist.vue", ()=>{
               }
             ],
             "liked": true
+          },
+          {
+            "id": 3,
+            "name": "Tofu süß-sauer mit Reis",
+            "image": "https://assets.zuckerjagdwurst.com/l0luiaj4bkj4bpodgx43k3w1nl0h/1110/701/55/true/center/tofusuesssauer-24.webp?animated=false",
+            "summary": "Vielleicht habt ihr von Tofu süß-sauer noch nie gehört, aber Hähnchen süß-sauer kennt sicher jeder – selbst wenn ihr es nicht gegessen gehabt, dann doch immerhin vom Lesen der Speisekarte eures chinesischen Lieblingsrestaurants. Das wichtigste an diesem Gericht ist aber auch nicht die Wahl des Proteins, sondern die Soße. Und deswegen funktioniert dieses Gericht auch wunderbar ohne Tier, aber mit Tofu. Obwohl ich so gerne frittiere, habe ich mich entschieden, eine Abendessen-freundlichere Variante des Gericht zu kochen. Statt den Tofu mit Teig zu ummanteln und goldbraun zu frittieren, werden die Tofuwürfel einfach in Speisestärke gewälzt und in der Pfanne angebraten. Das Ummanteln funktioniert übrigens super einfach, wenn ihr Tofu und Stärke zusammen in eine Tupperdose packt, den Deckel schließt und kräftig schüttelt. Um euch Extra-Abwasch zu ersparen, haben wir auch nur eine Pfanne für das Rezept verwendet, doch wenn ihr parallel Gemüse und Tofu in zwei Pfannen anbratet, steht euer Essen sogar noch früher auf dem Tisch. Für die Soße gibt es viele Varianten, in den klassischen Rezepten werden zum Beispiel Ketchup oder auch Pflaumensoße verwendet. Bei uns landet Orangensaft in der Soße, der fruchtig und leicht säuerlich ist. Tomatenmark und Sojasauce sorgen für eine geschmackliche Tiefe und Ingwer und Chiliflocken für eine leichte Schärfe. Wir immer könnt ihr aber ganz nach eigenem Geschmack würzen, mehr Schärfe, Säure oder auch Süße dazugeben.",
+            "ingredients": [
+              {
+                "id": 48,
+                "name": "Sesam zum Servieren",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 55,
+                "name": "Apfelessig",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 51,
+                "name": "Paprikaschoten",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 39,
+                "name": "Cashewnüsse zum Servieren",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 56,
+                "name": "Wasser",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 45,
+                "name": "Speisestärke",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 43,
+                "name": "Sesamöl zum Braten",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 44,
+                "name": "Sojasauce",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 58,
+                "name": "Tofu",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 52,
+                "name": "Salz",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 40,
+                "name": "Orangensaft",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 50,
+                "name": "Chiliflocken",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 59,
+                "name": "Tomatenmark",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 42,
+                "name": "Pfeffer",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 38,
+                "name": "Zwiebeln",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 47,
+                "name": "Ingwer",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 54,
+                "name": "brauner Zucker",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 53,
+                "name": "Frühlingszwiebeln",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 57,
+                "name": "Reis",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 46,
+                "name": "Walnüsse",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 49,
+                "name": "Speisestärke",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 41,
+                "name": "Sojasauce",
+                "amount": 0.0,
+                "unit": null
+              },
+              {
+                "id": 60,
+                "name": "Zucchini",
+                "amount": 0.0,
+                "unit": null
+              }
+            ],
+            "steps": [
+              {
+                "id": 23,
+                "number": 2,
+                "step": "Paprika und Zucchini in mundgerechte Stücke schneiden. Zwiebel klein schneiden und die Frühlingszwiebeln in feine Ringe schneiden. Den Reis erst unter fließendem Wasser abspülen und danach mit ca. 375 ml Wasser in einen Topf geben, leicht salzen und einmal aufkochen lassen. Danach mit Deckel ca. 15 Minuten kochen lassen, bis der Reis gar ist."
+              },
+              {
+                "id": 19,
+                "number": 4,
+                "step": "Nun die Zwiebel in die Pfanne geben und ca. 2-3 Minuten anschwitzen, bis sie glasig wird. Paprika und die Zucchini dazugeben, mit Salz und Pfeffer würzen und alles ca. 8-10 Minuten anbraten, bis das Gemüse weich, aber noch leicht knackig ist."
+              },
+              {
+                "id": 17,
+                "number": 6,
+                "step": "In der Zwischenzeit die Soße vorbereiten: Apfelessig, Orangensaft, braunen Zucker, Tomatenmark und Sojasauce verrühren. Frischen Ingwer direkt in die Schüssel reiben und Chiliflocken dazugeben. Alles gut verrühren. Wasser mit Stärke separat anrühren und dann ebenfalls zur Soße geben."
+              },
+              {
+                "id": 18,
+                "number": 5,
+                "step": "Parallel dazu ein paar Cashewkerne in einer fettfreien Pfanne rösten. Danach aus der Pfanne nehmen und klein hacken fürs Servieren später."
+              },
+              {
+                "id": 22,
+                "number": 8,
+                "step": "Tofu süß-sauer mit Reis servieren und zum Schluss mit Frühlingszwiebeln, Sesam und gerösteten Cashewkernen bestreuen."
+              },
+              {
+                "id": 21,
+                "number": 3,
+                "step": "Während der Reis kocht, Sesamöl in einer Pfanne erhitzen und zuerst den Tofu ca. 5 Minuten rundum anbraten, bis er gebräunt ist. Mit Sojasauce würzen und noch weitere 2-3 Minuten anbraten. Danach aus der Pfanne nehmen."
+              },
+              {
+                "id": 20,
+                "number": 1,
+                "step": "Tofu rundum mit Küchenpapier trocken tupfen und danach in größere Würfel schneiden. In eine Schüssel mit Speisestärke geben und alles gut durchschwenken, bis die Tofuwürfel rundum ummantelt sind. Das funktioniert auch toll in einer verschließbaren Schüssel: beides reingeben, Deckel drauf und gut durchschütteln."
+              },
+              {
+                "id": 16,
+                "number": 7,
+                "step": "In der Zwischenzeit die Soße vorbereiten: Apfelessig, Orangensaft, braunen Zucker, Tomatenmark und Sojasauce verrühren. Frischen Ingwer direkt in die Schüssel reiben und Chiliflocken dazugeben. Alles gut verrühren. Wasser mit Stärke separat anrühren und dann ebenfalls zur Soße geben."
+              }
+            ],
+            "liked": true
           }
         ]
       }
@@ -144,6 +333,6 @@ describe("testing Recipescardlist.vue", ()=>{
 
     //then
     const recipeCards = wrapper.findAllComponents(recipeCard)
-    expect(recipeCards.length).toBe(1)
+    expect(recipeCards.length).toBe(2)
   })
 })
